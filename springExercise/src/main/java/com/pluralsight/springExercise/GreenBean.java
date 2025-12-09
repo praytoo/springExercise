e4b856c7b465376ad1d5b6f7f6d7ae51152182d2
praytoo <praytoo52@icloud.com>
2025-12-09 14:26:42 -0800

    update

--- springExercise/src/main/java/com/pluralsight/springExercise/GreenBean.java
+++ springExercise/src/main/java/com/pluralsight/springExercise/GreenBean.java
@@ -1,10 +1,11 @@
 package com.pluralsight.springExercise;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GreenBean extends Bean{
-    public GreenBean(String recipe) {
+    public GreenBean(@Value("${greenbean.recipe}") String recipe) {
         super(recipe);
     }
 }

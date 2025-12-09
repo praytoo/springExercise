21d499166d205cdebff81a41c81d32845e7b8b9e
praytoo <praytoo52@icloud.com>
2025-12-09 14:24:14 -0800

    update

--- springExercise/src/main/java/com/pluralsight/springExercise/MagicBean.java
+++ springExercise/src/main/java/com/pluralsight/springExercise/MagicBean.java
@@ -1,13 +1,14 @@
 package com.pluralsight.springExercise;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Primary
 @Component
 public class MagicBean extends Bean{
-    public MagicBean(String recipe) {
+    public MagicBean(@Value("${magicbean.recipe}") String recipe) {
         super(recipe);
     }
 }
